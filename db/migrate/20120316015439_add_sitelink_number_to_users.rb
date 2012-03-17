12b6e3a6d6ec51a00dc0194e399d7773b7a6d9df
Thomas St.Onge <stongethomas@gmail.com>
2012-03-17 09:02:09 -0600

    fixed db now

--- db/migrate/20120316015439_add_sitelink_number_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddSitelinkIdToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :sitelink_id, :int
-  end
-
-  def self.down
-    remove_column :users, :sitelink_id
-  end
-end
